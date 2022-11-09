944a97c4c937df410b584de06c86be8545027f43
ianabreu <ianabreudev@gmail.com>
2022-11-09 05:56:08 -0300

    Criando Header e FabButton da tela ChatRoom

---
+++ src/components/FabButton/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+
+export default function FabButton({setVisible}) {
+	const handleNavigateButton = () => {
+		setVisible();
+	}
+
+	return (
+		<TouchableOpacity 
+		activeOpacity={0.9} 
+		style={styles.containerButton}
+		onPress={() => {handleNavigateButton}}
+		>
+			<View>
+				<MaterialIcons name='add' style={styles.text}/>
+			</View>
+		</TouchableOpacity>
+	);
+}
+
+const styles = StyleSheet.create({
+	containerButton: {
+		position: 'absolute',
+		bottom: '5%',
+		right: '6%',
+		justifyContent: 'center',
+		alignItems: 'center',
+
+		backgroundColor: '#21897E',
+
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+	},
+	text: {
+		fontSize: 35,
+		color: '#FFF',
+
+	}
+})
--- src/pages/ChatRoom/index.js
+++ src/pages/ChatRoom/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { auth } from '@react-native-firebase/auth'
+import React, { useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, FlatList, TouchableOpacity, View } from 'react-native';
+
+import auth from '@react-native-firebase/auth';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 import { useNavigation } from '@react-navigation/native'
 
+import FabButton from "../../components/FabButton";
+
 const ChatRoom = () => {
+
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleSignOut = () => {
+    auth()
+      .signOut()
+      .then(() => {
+        navigation.navigate('SignIn')
+      })
+      .catch(() => { console.log('nao tem usuario'); })
+  }
   return (
-    <View>
-      <Text>ChatRoom</Text>
-      <Button title='Login' onPress={()=> navigation.navigate('SignIn')}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerRoom}>
+        <View style={styles.headerRoomLeft}>
+          <TouchableOpacity onPress={handleSignOut}>
+            <MaterialIcons name='arrow-back' size={28} color={'#FFF'} />
+          </TouchableOpacity>
+          <Text style={styles.title}>Grupos</Text>
+
+        </View>
+
+        <TouchableOpacity>
+          <MaterialIcons name='search' size={28} color={'#FFF'} />
+        </TouchableOpacity>
+      </View>
+
+      <FabButton setVisible={()=> setModalVisible(true)}/>
+    </SafeAreaView>
   );
 }
 
 export default ChatRoom;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerRoom: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 40,
+    paddingBottom: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#21897E',
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  headerRoomLeft: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FFF',
+    paddingLeft: 10
+
+  }
+})
